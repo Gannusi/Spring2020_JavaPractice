fda1fe7712f69f2145692fc8f234f29e48fff384
Hanna Datsko <mega.anya22@gmail.com>
2020-04-22 00:37:44 -0400

    TestHAnna

--- src/day26_MethodOverLoadig/Method_OL_4.java
+++ src/day26_MethodOverLoadig/Method_OL_4.java
@@ -4,12 +4,15 @@ public class Method_OL_4 {
 
     public static void main(String[] args) {
         System.out.println("String");
+        System.out.println("Int");
+        System.out.println("Hanna");
     }
     public static void main(double[] args) {
         System.out.println("double");
     }
     public static void main(int[] args) {
         System.out.println("int");
+
     }
 
 
