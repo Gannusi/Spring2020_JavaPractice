ea42fd8815ebf3fc676bee8babf808d30bb613ee
Hanna Datsko <mega.anya22@gmail.com>
2020-04-27 10:43:27 -0400

    Dates

---
+++ src/day29_Wrapper_ArrayList/Auto_Boxing.java
@@ -0,0 +1,23 @@
+package day29_Wrapper_ArrayList;
+
+public class Auto_Boxing {
+    public static void main(String[] args) {
+
+         int a = 100;// primitive
+
+         Integer b = a;//Autoboxing
+
+
+       Byte by1= 98;
+
+       short sh1 = by1;//unboxing
+       byte b2 = by1;//unboxing
+       int i1 = by1;//unboxing
+
+
+
+
+
+
+    }
+}
