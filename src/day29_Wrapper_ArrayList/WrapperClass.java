c6c7ff8e2e729dba1e209605b4332f6bf4ffe299
Hanna Datsko <mega.anya22@gmail.com>
2020-04-27 10:29:52 -0400

    Day29 Class

---
+++ src/day29_Wrapper_ArrayList/WrapperClass.java
@@ -0,0 +1,31 @@
+package day29_Wrapper_ArrayList;
+
+public class WrapperClass {
+    public static void main(String[] args) {
+
+        int a = 100;//primitive
+
+        Integer b =200; //Wrapper class
+
+        short s1 =200;
+         int n1= s1;
+
+
+         Integer N1 =n1;
+
+
+
+         int a1 =20;
+         float f1 = a1;
+         Float f2 = f1;
+
+         Integer i1 = 200;
+         int a2 = i1;
+
+         Byte b1 = 126;
+         int a3= b1;
+        System.out.println( a3);
+
+
+    }
+}
