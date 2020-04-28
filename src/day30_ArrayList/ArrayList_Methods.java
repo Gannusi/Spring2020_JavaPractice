d07591504abe887ed83b89a5360bae63b8ec2e96
Hanna Datsko <mega.anya22@gmail.com>
2020-04-28 12:04:09 -0400

    day30_ArrayList

---
+++ src/day30_ArrayList/ArrayList_Methods.java
@@ -0,0 +1,52 @@
+package day30_ArrayList;
+
+import java.util.ArrayList;
+
+public class ArrayList_Methods {
+
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list1 = new ArrayList<>();
+
+        list1.add(5);//0
+        list1.add(7);//1
+        list1.add(8);//2
+        list1.add(1,6);
+
+        list1.set(3,9);
+
+        //list1.add(7,10); out of bound
+
+        System.out.println(list1);
+        int [] arr = {1, 2, 3, 4};
+        arr[3] =5;
+
+
+        ArrayList<String > list2 = new ArrayList<>();
+        list2.add("A");
+        list2.add("B");
+        list2.add(1,"C");
+        list2.add(1,"D");
+
+        list2.set(3,"F");
+        list2.set(2,"E");
+
+
+        System.out.println(list2);
+
+
+        System.out.println("===================================");
+
+        ArrayList<Integer> list3 = new ArrayList<>();
+        list3.add(1);
+        list3.add(2);
+        list3.add(3);
+        list3.add(4);
+
+
+
+
+
+
+    }
+}
