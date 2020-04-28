d307e1771bdce77df7d0846cb0ff4faf343e5bdb
Hanna Datsko <mega.anya22@gmail.com>
2020-04-28 13:12:30 -0400

    day30_ArrayList

--- src/day30_ArrayList/CombineTwoArrays.java
+++ src/day30_ArrayList/CombineTwoArrays.java
@@ -4,26 +4,33 @@ import java.util.ArrayList;
 
 public class CombineTwoArrays {
 
+ /*
+    4. write a program that can combine two String arrays into one arrayList
+            ex:
+                arr1 = {"A", "B", "C"};
+                arr2 = {"D", "E", "F", "G"};
+                list ==> {"A", "B", "C", "D", "E", "F", "G"}
+     */
 
     public static void main(String[] args) {
 
+        String[] arr1 = {"A", "B", "C"};
+        String[]  arr2 = {"D", "E", "F", "G", "H", "I"};
 
-        String [] arr1 = {"A", "B", "C"};
-        String [] arr2 = {"D", "E", "F", "G"};
+        ArrayList<String> list = new ArrayList<>();  //  {"A", "B", "C", "D", "E", "F", "G"}
 
-
-        ArrayList<String> list = new ArrayList<>();
-
-        for (int i =0; i< arr1.length; i++){
-
-           String eachEl=  arr1[i];
-           list.add(eachEl); // arr1[i]
-
+        for(int i=0; i < arr1.length ; i++ ){
+            String eachElement = arr1[i];
+            list.add( eachElement );  //arr1[i];
         }
-        for ( String eachEl: arr2){
 
-            list.add(eachEl);
+        for(String eachElement : arr2  ){
+            list.add(eachElement);
         }
+
+
+
+        System.out.println(list);
 
 
         System.out.println(list);
