7ab246f962990e1013427e6ca05092ae983b9742
Hanna Datsko <mega.anya22@gmail.com>
2020-04-30 10:51:17 -0400

    day31_BulkOperations

---
+++ src/homework/ReplIt/sort.java
@@ -0,0 +1,23 @@
+package homework.ReplIt;
+
+import java.util.Arrays;
+
+public class sort {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 5,3};
+
+        isSort(arr);
+    }
+
+    public static boolean isSort(int[] nums) {
+        int[] copy = new int [nums.length];
+        for(int i = 0; i < nums.length; i++){
+            copy [i] = nums [i];
+        }
+        Arrays.sort(nums);
+        boolean result = Arrays.equals(nums, copy);
+        System.out.println(result);
+        return result;
+    }
+}
